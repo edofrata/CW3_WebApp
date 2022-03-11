bf8cb84ce98fbe0f49eb8f166228c883e6c12bba
Edoardo Fratantonio <ef364@mdx.live.ac.uk>
2022-03-11 15:23:20 +0000

    Second commit

---
+++ js/components.js
@@ -0,0 +1,17 @@
+// components file 
+
+
+
+vueapp_2 = new Vue({
+
+    el: "#app2",
+    components: {
+        'my-component': {
+
+            props: [],
+            template: ''
+
+        }
+    },
+
+});
